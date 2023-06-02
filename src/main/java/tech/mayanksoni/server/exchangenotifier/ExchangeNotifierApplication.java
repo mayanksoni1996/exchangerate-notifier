57f4dd8139c1d2958a11f882d40598dd72c01b8a
Mayank Soni <mayank.soni@mayanksoni.tech>
2023-06-02 09:55:57 -0600

    Initial Commit

---
+++ src/main/java/tech/mayanksoni/server/exchangenotifier/ExchangeNotifierApplication.java
@@ -0,0 +1,13 @@
+package tech.mayanksoni.server.exchangenotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeNotifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeNotifierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/mayanksoni/server/exchangenotifier/ExchangeNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.mayanksoni.server.exchangenotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeNotifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
